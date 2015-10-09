791329d28af0eb7aa85b456ae83044f06b584e97
UTycwang <utycwang@gmail.com>
2015-10-09 00:29:34 -0500

    Data requesting in autocomplete passed

--- autocomplete/handlers/api_handlers.py
+++ autocomplete/handlers/api_handlers.py
@@ -13,7 +13,7 @@ WORD_LIST = ["Adnan", "Algorithm", "Austin", "Babel", "Car", "Dag", "Texas", "Va
 class AutoCompleteHandler(webapp2.RequestHandler):
     # return auto complete suggestions based on keywords
     def get(self):
-        keywords = str(self.request.get('term'))
+        keywords = str(self.request.get("keywords"))
         print ("AutoCompleteHandler: keywords is " + keywords)
         self.response.write(json.dumps(WORD_LIST, sort_keys=True))
 
--- autocomplete/scripts/autocomplete.js
+++ autocomplete/scripts/autocomplete.js
@@ -2,6 +2,18 @@ $(document).ready(function(){
     var cache = {};
     $("#search_key").autocomplete({
         minLength: 1,
-        source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ],
+        //source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ],
+	source: function(request, response){
+            cache = {};
+            var term = request.term;
+            if(term in cache){
+                response(cache[term]);
+                return;
+            }
+            $.getJSON("/api/autocomplete", {"keywords": term}, function(data, status, xhr){
+                cache[term] = data;
+                response(data);
+            })
+        }
     });
 });
